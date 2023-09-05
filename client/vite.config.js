ef5d397fad6f4d3abf5398f56b6b3ced2f025046
destocot <khurramcali@gmail.com>
2023-09-05 09:40:15 -0400

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/images': {
-        target: "https://shared-carousel-api.vercel.app/",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
