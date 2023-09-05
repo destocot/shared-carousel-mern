41c43f834bb4273a5b22f1efcc5a9a84f88f87b8
destocot <khurramcali@gmail.com>
2023-09-05 04:21:24 -0400

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["https://shared-carousel-demo.vercel.app/"],
+  origin: ["https://shared-carousel-demo.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true
 }))
