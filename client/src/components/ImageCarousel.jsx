e64a67f176d2275ea882bde3c4a58c13aaafce3c
destocot <khurramcali@gmail.com>
2023-09-05 09:07:25 -0400

    update 01

--- client/src/components/ImageCarousel.jsx
+++ client/src/components/ImageCarousel.jsx
@@ -11,7 +11,7 @@ export default function ImageCarousel() {
   useEffect(() => fetchImages, []);
 
   const fetchImages = () => {
-    fetch("https://shared-carousel-api.vercel.app/api/images", {
+    fetch("api/images", {
       method: "GET",
     })
       .then((res) => {
